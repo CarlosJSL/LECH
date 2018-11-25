26f2f1aa4bcaf89fefbc5fae7405b678dacd6291
Bianca Santiago <bianca.santiago72@gmail.com>
2018-11-25 04:17:10 +0000

    criando arquivo de chamada PHP e adicionando navbar

---
+++ includes/navbar.php
@@ -0,0 +1,35 @@
+<section>
+<nav id="mainNav"> 
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center"> 
+				<div class="mobile-display"> 
+					<a href="index.html"><img src="assets/images/logo.png" alt="Logo" width="64px" height="auto"></a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-offset-2 col-lg-10 col-md-offset-1 col-md-11 col-sm-12 col-xs-12">
+				<!-- Main menu Area start -->
+				<div class="main-menu">
+					<div class="navbar-collapse">
+						<a href="#" class="toggle-menu">☰</a>
+						<ul class="nav-list nav-open" id="nav">
+							<li class="current"><a class="js-scroll-trigger" href="#home">Home</a></li>
+							<li><a class="js-scroll-trigger" href="#olech">O LECH</a></li>
+							<li><a class="js-scroll-trigger" href="#Ensino">Ensino</a></li>
+							<!-- Logo Area start -->
+							<li class="logo"><a href="index.html"><img src="assets/images/logo.png" alt="Logo" width="64px" height="auto"></a></li>
+							<!-- Logo Area end -->
+							<li><a class="js-scroll-trigger" href="#pesquisa">Pesquisa</a></li>
+							<li><a class="js-scroll-trigger" href="#publicacoes">Publicações</a></li>
+							<li><a class="js-scroll-trigger" href="#galeria">Galeria</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- Main menu Area end -->
+			</div>
+		</div>
+	</div>
+</nav>
+</section>
---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LECH</title>
+    
+    <link rel="stylesheet" type="text/css" media="screen" href="./node_modules/bootstrap/dist/css/bootstrap.min.css" />
+    
+    <link rel="stylesheet" type="text/css" media="screen" href="./node_modules/bootstrap/dist/js/bootstrap.min.js" />
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/index.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap/jquery.lineProgressbar.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap/owl.carousel.css" rel="stylesheet">
+    <link href="assets/css/bootstrap/magnific-popup.css" rel="stylesheet">
+</head>
+
+<body style="background-color: #e8e8e8">
+
+        <div class="preloader">
+            <img class="preloader-img" src="view/images/any/preload.gif" />
+        </div>
+
+        <!-- Navbar -->
+        <?php include 'includes/navbar.php' ?>
+        <!-- Navbar End  -->
+
+        <?php
+        switch($_GET['page']){
+            	case 'home':
+            	   include 'includes/home.php';
+            	   break;
+            	case 'olech':
+            	   include 'includes/olech.php';
+            	   break;
+            	case 'ensino':
+                include 'includes/ensino.php';
+            	   break;
+            case 'pesquisa':
+                include 'includes/pesquisa.php';
+                break;
+            	case 'publicacoes':
+            	   include 'includes/publicacoes.php';
+            	   break;
+            	case 'galeria':
+            	   include 'includes/galeria.php';
+            	   break;
+            	case 'contato':
+            	   include 'includes/contato.php';
+            	   break;
+             default:
+            	   include 'includes/initial.php';
+            	   break;
+            }
+            ?>
+
+        <!-- Footer -->
+        <?php include 'includes/footer.php' ?>
+        <!-- Footer End -->
+<!--
+        <nav class="navbar navbar-expand-lg text-center">
+        <div class="container text-center">
+          <a class="navbar-brand text-center" href="#"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto mx-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#lech"> Lech</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#lech"> Ensino </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#lech"> Pesquisa</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#lech"> Publicações </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#lech"> Galeria</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    <div id="app"></div>
+
+        <footer class="footer navbar-fixed-bottom">s</footer>
+
+-->
+
+<!-- ###################################### JS ###################################### -->
+      <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCs6_DQBH230AV6t9T-NBIwcqfJOLNKssc"type="text/javascript"></script>
+      <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    	<script src="./node_modules/popper.js/dist/popper.min.js"></script>
+	    <script src="js/route.js"></script>
+	    <script src="js/router.js"></script>
+	    <script src="js/app.js"></script>
+
+    </body>
+</html>
